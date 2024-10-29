bb1ef2ef4401ff9553f21d17367459475b63bc58
alisonmjn <162391197+alisonmjn@users.noreply.github.com>
2024-10-29 18:21:35 -0500

    Widgets Container, Text, Row

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Hola Mundo',home: Container(child: const Text('Hola Mundo'),));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hola Mundo',
+        home: RecipeBook());
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(
+          'Recipe Book',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(width: MediaQuery.of(context).size.width, height: 125, child: Card(child: Row(children: <Widget>[
+        Container(
+          height: 125,
+          width: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(), 
+          ),
+        ),
+        SizedBox(width: 26,),
+        Column(children: <Widget>[
+          Text('Lasagna'),
+          Text('Alison J'),
+          Container(
+            height: 2,
+            width: 75,
+            color: Colors.orange,
+          )
+        ],)
+      ],),),),
+    );
   }
 }
