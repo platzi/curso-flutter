55a242bee5b1204aa04f87e60aceee237dc27462
alisonmjn <162391197+alisonmjn@users.noreply.github.com>
2024-10-29 19:31:43 -0500

    styles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,39 +6,52 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 125,
-          child: Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  height: 125,
-                  width: 100,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(),
+      body: Column(children: <Widget>[
+        _RecipesCard(context),
+        _RecipesCard(context),
+      ],)
+    );
+  }
+
+  Widget _RecipesCard(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 125,
+            child: Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 125,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.orange
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 26,
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('Lasagna'),
-                    Text('Alison J'),
-                    Container(
-                      height: 2,
-                      width: 75,
-                      color: Colors.orange,
-                    )
-                  ],
-                )
-              ],
+                  SizedBox(
+                    width: 26,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Lasagna', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
+                      SizedBox(height: 4,),
+                      Container(
+                        height: 2,
+                        width: 75,
+                        color: Colors.orange,
+                      ),
+                      Text('Alison J', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand')),
+                      SizedBox(height: 4,),                      
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-        ),
     );
-    
   }
 }
