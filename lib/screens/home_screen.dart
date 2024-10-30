0aeb84e1974b44c21841a7ac907830719d8ea5e6
alisonmjn <162391197+alisonmjn@users.noreply.github.com>
2024-10-30 14:37:23 -0500

    Bottom and TextField widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,52 +6,131 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        _RecipesCard(context),
-        _RecipesCard(context),
-      ],)
+      body: Column(
+        children: <Widget>[
+          _RecipesCard(context),
+          _RecipesCard(context),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orange,
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            _showBottom(context);
+          }),
     );
   }
 
-  Widget _RecipesCard(BuildContext context){
+  Future<void> _showBottom(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (contexto) => Container(
+              width: MediaQuery.of(context).size.width,
+              height: 500,
+              color: Colors.white,
+              child: RecipeForm(),
+            ));
+  }
+
+  Widget _RecipesCard(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 125,
-            child: Card(
-              child: Row(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 26,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Container(
-                    height: 125,
-                    width: 100,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.network('https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg', fit: BoxFit.cover,),
-                    ),
+                  Text(
+                    'Lasagna',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
                   ),
                   SizedBox(
-                    width: 26,
+                    height: 4,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Lasagna', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
-                      SizedBox(height: 4,),
-                      Container(
-                        height: 2,
-                        width: 75,
-                        color: Colors.orange,
-                      ),
-                      Text('Alison J', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand')),
-                      SizedBox(height: 4,),                      
-                    ],
-                  )
+                  Container(
+                    height: 2,
+                    width: 75,
+                    color: Colors.orange,
+                  ),
+                  Text('Alison J',
+                      style: TextStyle(fontSize: 16, fontFamily: 'Quicksand')),
+                  SizedBox(
+                    height: 4,
+                  ),
                 ],
-              ),
-            ),
+              )
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeForm extends StatelessWidget {
+  const RecipeForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(8),
+        child: Form(
+          //key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Add New Recipe',
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              _buildTextField(label: 'Recipe Name')
+            ],
+          ),
+        ));
+  }
+
+  Widget _buildTextField({required String label}) {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(
+            fontFamily: 'Quicksand',
+            color: Colors.orange,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.orange, width: 1),
+              borderRadius: BorderRadius.circular(10))),
     );
   }
 }
